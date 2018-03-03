72a3bf35146dcb03fa11b9a0eb43c2c338d2c71f
ArsenijeYTS23 <36597375+ArsenijeYTS23@users.noreply.github.com>
2018-03-03 22:16:07 +0100

    Add files via upload

---
+++ new.blade.php
@@ -0,0 +1,25 @@
+Ja sam covek, a ovo su neke moje karakteristike...<br>
+bracni status: {{ $covek->brak->name }}<br>
+deca: {{ $covek->deca->name }}<br>
+godine: {{ $covek->godine->name }}<br>
+mesto u kome zivim: {{ $covek->mesto->name }}<br>
+muzika koju slusam: 
+@foreach($covek->muzika as $m)
+ @if ($loop->last)
+ {{ $m->name }}<br>
+ @break
+ @endif
+{{ $m->name }} i
+@endforeach
+moje obrazovanje: {{ $covek->obrazovanje->name }}<br>
+politicko opredeljenje: {{ $covek->politika->name }}<br>
+moja trenutna primanja: {{ $covek->primanja->name }} hiljada <br>
+moja veroispovest: 
+@if($covek->veroispovest->name=='ateista')
+{{ '/' }}
+@endif
+
+
+
+
+
